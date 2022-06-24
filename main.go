1db51776c3b459ef586d289474177d8e7504d451
Mashiyat Hussain <mashiyat.hussain@hypd.in>
2022-06-24 13:37:02 +0530

    Old changes

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import "net/http"
+
+// Write an http web server that returns an html header that shows the user's IP address and the time.
+// Usage:
+// go run examples/web/httpserverwithhtml.go
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello, world!</h1>"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
